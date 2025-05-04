de94dae4efc50121119b6b75031944833038e926
xbelderol220000001002 <xbelderol_220000001002@uic.edu.ph>
2025-05-04 12:45:38 +0800

    initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LandingPage from './LandingPage.jsx';
+import './styles/LandingPage.css';
+
+
+function App() {
+  return (
+    <div>
+      <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
